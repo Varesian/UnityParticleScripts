08912dc1f170cc48a544ad67381e6025f48c6742
Cory Barr <cory.barr@gmail.com>
2012-08-19 13:01:31 -0700

    initial commit

---
+++ AttractParticlesByDistance.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class AttractParticlesByDistance : MonoBehaviour {
+	
+	public ParticleEmitter pe;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pe.worldVelocity = transform.position;
+	}
+}
